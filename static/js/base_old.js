66a0e2f5624fa13b22a2236b2efd8a3faeaeab13
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-08-13 14:19:12 +0400

    Add static

---
+++ static/js/base_old.js
@@ -0,0 +1,152 @@
+var map = L.map('map');
+map.setView([34.76, 113.65], 11);
+
+/*L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);*/
+var night = L.tileLayer.provider('Jawg.Dark', {
+        variant: 'c603bda4-6780-4f3a-b06f-b5de44f2ed88',
+        accessToken: 'Ej2LIAfNJgZZ1TprVGdK16SXWDk9sWDrFbI8YMNYfIPYJMLwsN43j0dqVXovhUQE'
+        })
+    daytime = L.tileLayer.provider('Jawg.Light', {
+        accessToken: 'Ej2LIAfNJgZZ1TprVGdK16SXWDk9sWDrFbI8YMNYfIPYJMLwsN43j0dqVXovhUQE'
+        })
+
+map.addLayer(night)
+var overlays = {
+    "Light Theme": daytime
+};
+
+L.control.layers(null, overlays).addTo(map);
+
+var sidebar = L.control.sidebar('sidebar', {
+  closeButton: true,
+  position: 'right'
+});
+map.addControl(sidebar);
+
+sidebar.show();
+
+
+function loadJSON(elementId){
+  let element = document.getElementById(elementId);
+
+  if (!element){
+    return null;
+  }
+
+  return JSON.parse(element.textContent);
+}
+
+let places = loadJSON('places-geojson');
+
+L.geoJSON(places, {
+    pointToLayer: function(geoJsonPoint, latlng) {
+      if (geoJsonPoint.geometry.type != "Point"){
+        return
+      }
+
+      let color = geoJsonPoint.properties.color || 'red';
+
+      var pulsingIcon = L.icon.pulse({
+        iconSize: [8, 8],
+        color: color,
+        fillColor: color,
+        heartbeat: 2.5,
+      });
+
+      let marker = L.marker(latlng, {
+        icon: pulsingIcon,
+        riseOnHover: true,
+      });
+
+      marker.bindTooltip(geoJsonPoint.properties.title);
+      marker.bindPopup(function (layer) {
+        return geoJsonPoint.properties.title;
+      })
+
+      marker.on('click', function(event){
+        sidebar.show();
+        loadPlaceInfo(geoJsonPoint.properties.placeId, geoJsonPoint.properties.detailsUrl);
+      });
+      return marker;
+    }
+}).addTo(map);
+
+var sidebarApp = new Vue({
+  el: '#sidebar-app',
+  data: {
+    loadingPlaceId: null,
+    selectedPlace: null,
+  },
+  computed: {
+    promptVisible: function () {
+      return !this.loading && !this.selectedPlace;
+    },
+    loading: function () {
+      return this.loadingPlaceId !== null;
+    },
+
+    carouselImages: function () {
+      if (!this.selectedPlace || !this.selectedPlace.images.length){
+        return [];
+      }
+      return this.selectedPlace.images;
+    },
+  },
+  updated: function () {
+    this.$nextTick(function () {
+      // Код, будет запущен только после обновления всех представлений
+      $('#place-photos').carousel();
+    })
+  },
+  methods: {
+    handlePhotosClick: function(slideId='next') {
+      // default event handlers of Bootstrap Carousel conflict with Leaflet
+      // so custom handler will mimic expected carousel behaviour
+      $('#place-photos').carousel(slideId);
+    }
+  },
+});
+
+map.on('click', function () {
+  sidebarApp.selectedPlace = null;
+  sidebarApp.loadingPlaceId = null;
+})
+
+async function loadPlaceInfo(placeId, detailsUrl){
+  sidebarApp.selectedPlace = null;
+  sidebarApp.loadingPlaceId = placeId;
+
+  try {
+    let response = await fetch(detailsUrl);
+    if (!response.ok){
+      return;
+    }
+
+    let data = await response.json();
+
+    if (sidebarApp.loadingPlaceId != placeId){
+      // Place loading was cancelled by user
+      return
+    }
+
+    sidebarApp.selectedPlace = {
+      title: data.title,
+      placeId: placeId,
+      images: data.images || [],
+      category: data.category,
+      description: data.description,
+      address: data.address,
+      pinyin_address: data.pinyin_address,
+      average_price: data.average_price,
+      rating: data.rating,
+      police_rating: data.police_rating,
+      events: data.events,
+      reviews: data.reviews,
+
+    };
+  } finally {
+    if (sidebarApp.loadingPlaceId == placeId){
+      sidebarApp.loadingPlaceId = null;
+    }
+  }
+}
