53f4e660fab1b9d4860bb2b902d5f1f32821869a
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-08-13 14:27:35 +0400

    Get JWT token on authorization

--- izhevsk/urls.py
+++ izhevsk/urls.py
@@ -2,13 +2,24 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from apps.api.routers import router
+from apps.main.views import MainViewSet
 from izhevsk import settings
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    path('', MainViewSet.as_view({'get': 'list'})),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+]
+
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if settings.DEBUG:
